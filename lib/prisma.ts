1b40a1d7d6db6608121381ec4eecdd91983da23c
A Trilha do E-Commerce <atrilhadoecommerce@gmail.com>
2026-04-01 19:22:35 -0300

    perf: disable prisma query logs for cleaner development environment

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -30,8 +30,6 @@ if (checkStale()) {
 
 export const prisma =
     globalForPrisma.prisma ||
-    new PrismaClient({
-        log: ['query'],
-    });
+    new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
